d3471e417b1b00a3360f01bc1d3d5531708a49d8
frazse <djozyff@gmail.com>
2025-12-03 14:32:28 +0100

    Create get-abit-more-data.js

---
+++ Override-Breakpoint/get-abit-more-data.js
@@ -0,0 +1,52 @@
+In the file BJ3MmXx5.js
+Search for "humanRiderPositions.set(o)"
+Right-click on linenumber which in this case is just - and click "Add Conditional Breakpoint"
+Paste the code below, change SET_YOUR_NAME to yourown name and hit enter
+
+(if you do this you need to keep the console open for the data to update.)
+
+ Another alternative is to right-click BJ3MmXx5.js and use override content, aka we download and use a local copy of the javascript file.
+In that file we;
+Change this: humanRiderPositions.set(o)
+To this: window.hackedRiders=this.humansList;humanRiderPositions.set(o)
+Save the file and reload the page.
+(I have not tried this so ymmv.)
+
+
+
+###################################################
+// 1. Get the list of other players
+const others = this.humansList || [];
+
+// 2. Get YOUR player (focalRider)
+const me = this.focalRider;
+
+// 3. Combine them into one array
+// (We check if 'me' exists just in case you are in spectator mode)
+const allRiders = me ? [me, ...others] : others;
+
+// 4. Map the combined list to our clean format
+window.hackedRiders = allRiders.map(r => {
+    const c = r.config || {};
+    
+    // Combine names
+    const f = c.first_name || "";
+    const l = c.last_name || "";
+    let fullName = (f + " " + l).trim();
+    
+    // If name is empty, check if it's YOU
+    if (!fullName && r === me) fullName = "SET_YOUR_NAME";
+
+    return {
+        name: fullName || ("Rider " + (r.id || "?")),
+        dist: r.currentPathDistance,
+        speed: r.speed,
+        power: r.power,
+        x: r.position.x,
+        z: r.position.z,
+        // Add a flag so we can color your own name differently later if we want
+        isMe: (r === me) 
+    };
+});
+
+false; // Don't pause!
