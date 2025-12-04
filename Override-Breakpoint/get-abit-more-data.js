83407c14286da5d2d449d7e57497a5ced5aa6f08
frazse <djozyff@gmail.com>
2025-12-04 13:41:51 +0100

    Update get-abit-more-data.js

--- Override-Breakpoint/get-abit-more-data.js
+++ Override-Breakpoint/get-abit-more-data.js
@@ -4,25 +4,7 @@ Right-click on linenumber which in this case is just - and click "Add Conditiona
 Paste the code below, change SET_YOUR_NAME to yourown name and hit enter
 
 (if you do this you need to keep the console open for the data to update.)
-
- Another alternative is to right-click BJ3MmXx5.js and use override content, aka we download and use a local copy of the javascript file.
-In that file we;
-Change this: humanRiderPositions.set(o)
-To this: window.hackedRiders=this.humansList;humanRiderPositions.set(o)
-Save the file and reload the page.
-(I have not tried this so ymmv.)
-
- 
-This is what it should give you access to:
-
-Identity	r.config.first_name, r.config.last_name	Name String	Player Name / "ME" flag
-Performance	r.power	Raw Watts	Power Output
-Physics	r.speed	Speed (m/s)	Used to calculate km/h
-Position	r.currentPathDistance	Route Distance (meters)	Total distance traveled (for sorting)
-Position	r.position.x, r.position.z	3D Coordinates	Location in the game world
-Configuration	r.config.weight	Rider Weight (kg)	Used to calculate Watts/kg <- Other riders, not yours
-
-
+You can also use Override/local-override but I havent tried that so ymmv
 ###################################################
 /* FINAL LOCATION-INDEPENDENT BREAKPOINT CODE */
 /* FINAL, GUARANTEED BREAKPOINT CODE (V4.0 - FIXES STARTUP RESET) */
